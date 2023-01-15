d3a75e1ef34dcd094953d5b1e2d88c8f5ca1a903
unknown <cuentaparamooc@gmail.com>
2023-01-14 21:33:44 -0500

    Clase 20 de 27 Integrar el shopping cart

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
+// DESKTOP MENU
 const menuEmail = document.querySelector (".navbar-email");
 const desktopMenu = document.querySelector (".desktop-menu");
-
-const burguerMenu = document.querySelector(".menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-
 menuEmail.addEventListener('click', toggleDesktopMenu);
-burguerMenu.addEventListener('click', toggleMobileMenu);
-
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+  aside.classList.add("inactive");    
+  desktopMenu.classList.toggle('inactive');
 }
 
+
+// MOBILE MENU
+const burguerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+burguerMenu.addEventListener('click', toggleMobileMenu);
+
 function toggleMobileMenu(){
+    aside.classList.add("inactive");    
     mobileMenu.classList.toggle('inactive');
+}
+
+
+// SHOPPING CART
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+shoppingCart.addEventListener('click', toggleAside);
+
+function toggleAside(){
+  desktopMenu.classList.add("inactive");
+  mobileMenu.classList.add("inactive"); 
+  aside.classList.toggle("inactive")   
 }
